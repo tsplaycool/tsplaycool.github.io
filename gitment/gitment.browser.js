ae3e088d660b8e657a03723a21a49e2a47efd92a
shuai.tan <916759791@qq.com>
2019-01-03 20:47:02 +0800

    Site updated: 2019-01-03 20:47:01

--- gitment/gitment.browser.js
+++ gitment/gitment.browser.js
@@ -149,7 +149,7 @@ var Gitment =
                 invariant(false, getMessage("m001"));
             }, false, false);
             var action = function action(arg1, arg2, arg3, arg4) {
-                if (arguments.length === 1 && typeof arg1 === "function") return createAction(arg1.name || "<unnamed action="">", arg1);
+                if (arguments.length === 1 && typeof arg1 === "function") return createAction(arg1.name , arg1);
                 if (arguments.length === 2 && typeof arg2 === "function") return createAction(arg1, arg2);
                 if (arguments.length === 1 && typeof arg1 === "string") return namedActionDecorator(arg1);
                 return namedActionDecorator(arg2).apply(null, arguments);
@@ -175,7 +175,7 @@ var Gitment =
                 };
             }
             function runInAction(arg1, arg2, arg3) {
-                var actionName = typeof arg1 === "string" ? arg1 : arg1.name || "<unnamed action="">";
+                var actionName = typeof arg1 === "string" ? arg1 : arg1.name ;
                 var fn = typeof arg1 === "function" ? arg1 : arg2;
                 var scope = typeof arg1 === "function" ? arg2 : arg3;
                 invariant(typeof fn === "function", getMessage("m002"));
@@ -3707,4 +3707,4 @@ var Gitment =
 
         /***/ })
     /******/ ]);
-//# sourceMappingURL=gitment.browser.js.map</=></=></unnamed></not></unnamed>
+//# sourceMappingURL=gitment.browser.js.map</=></=></not>
